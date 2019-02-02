28a341a99bec4ed9013e935abad432fd8bdfce22
Jay Cheroske <jay@portal.org>
2019-02-02 14:23:32 -0800

    testing lib

--- test/shared.js
+++ test/shared.js
@@ -1,5 +1,13 @@
 console.log('beginning load of testing lib');
 
+/*
+ * Common functions
+ */
+
+
+/*
+ * Status code validation
+ */
 statusIs = code =>
   pm.test(`Status code is ${code}`, () => pm.response.to.have.status(code));
 
@@ -12,5 +20,42 @@ forbidden = () => statusIs(403);
 notFound = () => statusIs(404);
 
 serviceUnavailable = () => statusIs(503);
+
+
+/*
+ * JSON RPC validation
+ */
+const Ajv = require('ajv');
+const ajv = new Ajv({logger: console});
+
+const jsonRpcSchema = {
+  type: 'object',
+  properties: {
+    id: {
+      oneOf: [
+        {
+          type: 'number',
+        },
+        {
+          type: 'string',
+        }
+      ],
+    }
+  },
+  required: [
+    'id',
+    'jsonrpc',
+  ],
+  oneOf: [
+    {
+      required: ['result']
+    },
+    {
+      required: ['errror']
+    }
+  ]
+}
+
+validJsonRpc = pm.test(`Response is valid JSON RPC`, () => pm.response.to.have.jsonSchema(jsonRpcSchema));
 
 console.log('finished load of testing lib');
