f05a9b6582933efaa4e2a3c07f7bcc059d60ede0
Jay Cheroske <jay@portal.org>
2019-02-02 13:37:20 -0800

    testing lib

--- test/shared.js
+++ test/shared.js
@@ -9,7 +9,7 @@ const badRequest = () => statusIs(400)
 
 const forbidden = () => statusIs(403)
 
-const notFound = () => statusIs(404)
+notFound = () => statusIs(404)
 
 const serviceUnavailable = () => statusIs(503)
 
