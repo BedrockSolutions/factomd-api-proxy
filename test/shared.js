fa6ba0acb80d063af0aa57312792ecf759418568
Jay Cheroske <jay@portal.org>
2019-02-02 13:40:32 -0800

    testing lib

--- test/shared.js
+++ test/shared.js
@@ -1,18 +1,18 @@
-console.log('beginning load of testing lib')
+console.log('beginning load of testing lib');
 
 const statusIs = code =>
-  pm.test(`Status code is ${code}`, () => pm.response.to.have.status(code))
+  pm.test(`Status code is ${code}`, () => pm.response.to.have.status(code));
 
-const ok = () => statusIs(200)
+const ok = () => statusIs(200);
 
-const badRequest = () => statusIs(400)
+const badRequest = () => statusIs(400);
 
-const forbidden = () => statusIs(403)
+const forbidden = () => statusIs(403);
 
-notFound = () => statusIs(404)
+notFound = () => statusIs(404);
 
-const serviceUnavailable = () => statusIs(503)
+const serviceUnavailable = () => statusIs(503);
 
-const isTestLibLoaded = true
+const isTestLibLoaded = true;
 
-console.log('finished load of testing lib')
+console.log('finished load of testing lib');
