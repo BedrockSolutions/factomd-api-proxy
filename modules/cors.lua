82bbe4070bd11079e319908792b2c23f44f92751
Jay Cheroske <jay@portal.org>
2019-02-08 10:46:10 -0800

    fixed some empty string checks

--- modules/cors.lua
+++ modules/cors.lua
@@ -47,7 +47,7 @@ local function handle_options(arg)
   local origin = get_header('Origin')
 
   -- Check that the Origin header is present
-  if not origin then
+  if not origin or origin == '' then
     set_response_error{response=response, code=codes.CORS_ERROR, message='Origin header missing', status=ngx.HTTP_NOT_FOUND}
     return
   end
@@ -101,7 +101,7 @@ local function handle_get_and_post(arg)
 
   local origin = get_header('Origin')
 
-  if not origin then
+  if not origin or origin == '' then
     return
   end
 
