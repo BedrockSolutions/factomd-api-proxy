aa4306595551f37791c04e27c44cb4ea70f2bb16
Jay Cheroske <jay@portal.org>
2019-10-20 15:09:27 -0700

    bin script edits

--- bin/run_factomd.sh
+++ bin/run_factomd.sh
@@ -8,6 +8,6 @@ docker run -d --name factomd \
   -p 8088:8088 -p 8090:8090 -p 8108:8108 \
   -v $(pwd)/scratch/factomd/config:/app/config \
   -v $(pwd)/scratch/factomd/config:/app/config \
-  bedrocksolutions/factomd:FD-689_test0
+  bedrocksolutions/factomd:v6.4.3-alpine
 
 docker logs -f factomd
